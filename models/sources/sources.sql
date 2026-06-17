c92d3f5df4c66ab734e40c7270c459f0f6083642
Muhammad Hasbi <muhammad.hasbi@cloud-kinetics.com>
2026-06-17 17:29:10 +0700

    Change query cast all results from VARCHAR(MAX) to TEXT
     On branch feature/update-dbt-artifacts
     Changes to be committed:
    	modified:   models/sources/exposures.sql
    	modified:   models/sources/models.sql
    	modified:   models/sources/seeds.sql
    	modified:   models/sources/snapshots.sql
    	modified:   models/sources/sources.sql
    	modified:   models/sources/tests.sql

--- models/sources/sources.sql
+++ models/sources/sources.sql
@@ -19,7 +19,7 @@ select
     {% if target.type == "snowflake" %}, cast(null as {{ type_array() }}) as freshness
     {% else %}, cast(null as {{ type_json() }}) as freshness
     {% endif %},
-    cast(null as VARCHAR(MAX)) as all_results
+    cast(null as TEXT) as all_results
 from dummy_cte
 where 1 = 0
 
